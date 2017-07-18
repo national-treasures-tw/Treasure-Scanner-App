68b3db12ef0808b274b617146c45aaa2518f3d6c
Hsin <shinsonh@gmail.com>
2017-07-18 15:51:41 -0400

    add license

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ import {
 const SBScanbot = NativeModules.SBScanbot;
 
 // Set valid license here
-SBScanbot.setLicense('');
+SBScanbot.setLicense('Px/ikVYDSTzigYnpa+pwcXluzh/sk6B+16D2zqRf2fK2HgvBDRI6ofHV+DmVSLJMgcXprW7h4bEMKkczYth3SsPeV7B0CLQJdnblxXTnC/DAAFmJxQMK+0Icl9deZWuzeZW/YDT4fCvQRLAFgaFLQKWzYzBmoZj+Sanl0R5OOdG+/thIvTQMXJF+vSvW3NGQzr1ADUKsZ8ye3O5ERLKsMtQo+kAMA/krKVPpMStHN+8lP+CU1Qb4Z7cWjSjCcqIBT3HS5e3oPDqDrp9Spy81XXYfr/KTlRIT9G7ZZIsi3650tpB1KE3zJvUzolBlKMuUVNpGHC4NjFyBUbPU6mS9Ow==\nU2NhbmJvdFNESwpUTlQuVGFpd2FuLVRyZWF1c3JlCjE1MDE4OTExOTkKNzgKMQ==\n');
 
 export default class ScanbotExample extends Component {
 
